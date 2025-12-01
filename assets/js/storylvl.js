a7d904be59679c5d3bada86230d2625fe8734cd3
Anthony Valenzo <94491552+avalenzo1@users.noreply.github.com>
2025-11-30 23:33:55 -0600

    idek gng 😔✌️💔

--- assets/js/storylvl.js
+++ assets/js/storylvl.js
@@ -1,13 +1,25 @@
 function setup() {
     createCanvas(800, 600);
-    
+
+    displayMode('normal', 'pixelated');
+
+    santaSleigh = new Sprite(width / 2, height / 2, 144, 64);
+    santaSleigh.image = "/assets/images/sprites/santaSleigh.png"
+    santaSleigh.scale = 2;
     initializeSprites();
 }
 
+let santaSleigh;
+let bellSnd;
+let fortniteSnd;
+let explosionSnd;
+let explosionGif;
+let playExplosion = false, playedExplosion = false; // present and future tense
+
 const dialogue = [
     "Press the [Enter] key to continue.",
-    "Santa is dumb sjdfijblasbdjfihsjdhfbiksdhbfk",
-    "hriasehbfisbhdf9bsdifbsodbfib",
+    "Santa was blah blah blah",
+    "but Santa is dumb sjdfijblasbdjfihsjdhfbiksdhbfk",
     "idk",
     "Santa's elf and and Rudolph must now save Christmas!"
 ];
@@ -19,9 +31,40 @@ function mouseClicked() {
 
 }
 
-function draw() {
-    background(255);
+function preload() {
+explosionSnd = loadSound('/assets/sounds/explosion.mp3');
+explosionGif = loadImage('/assets/images/sprites/explosion.gif');
+fortniteSnd = loadSound('/assets/sounds/death.mp3');
+bellSnd = loadSound('/assets/sounds/bell.mp3');
+}
 
+let cloudX = 0;
+let cloudY = 0;
+
+function draw() {
+    background(235);
+
+    ellipse(200 + cloudX, 200 + cloudY, 300, 100);
+
+    ellipse(500 + cloudX, 300 + cloudY, 300, 100);
+
+    if (currentLine < 3)
+    santaSleigh.vel.y = cos(frameCount * 2.2) * 2;
+
+    cloudX -= 2;
+
+    if (cloudX < -700) {
+        cloudX = width;
+    }
+
+
+    if (playedExplosion) {
+        image(explosionGif, width - 600, height / 2, 400, 400);
+        explosionGif.play();
+    }
+}
+
+function drawDialogue() {
     fill("#2a2a2aff");
     rect(0, height - 80, width, 80);
     fill("#fff");
@@ -36,9 +79,45 @@ function draw() {
     if (kb.pressed('Enter')) {
         if (currentLine < dialogue.length - 1) {
             currentLine++;
+
+            if (currentLine == 2) {
+                if (!bellSnd.isPlaying()) {
+                    bellSnd.play();
+                }
+
+                santaSleigh.image = "/assets/images/sprites/santaSadSleigh.png"
+                santaSleigh.vel.y = 0;
+            }
+
+            if (currentLine == 3) {
+                santaSleigh.physics = DYNAMIC;
+                santaSleigh.rotationSpeed = 1;
+                world.gravity.y = 10;
+                world.gravity.x = 2;
+
+                if (!playExplosion) {
+                    setTimeout(() => {
+                        explosionSnd.play();
+                        playedExplosion = true;
+
+                        setTimeout(() => {
+fortniteSnd.play();
+                        }, 200);
+                    }, 1000);
+
+                    playExplosion = true;
+                }
+            }
+
             currentChar = 0;
         } else {
             window.location.href = "/adrianlvl.html";
         }
     }
+}
+
+function drawFrame() {
+    allSprites.draw();
+
+    drawDialogue();
 }
