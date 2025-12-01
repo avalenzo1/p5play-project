06b240cb04d8f5ea4af63f347f22ce0a17ca345f
Anthony Valenzo <94491552+avalenzo1@users.noreply.github.com>
2025-11-30 22:25:26 -0600

    finished menu screen and narrator screen

---
+++ assets/js/storylvl.js
@@ -0,0 +1,44 @@
+function setup() {
+    createCanvas(800, 600);
+    
+    initializeSprites();
+}
+
+const dialogue = [
+    "Press the [Enter] key to continue.",
+    "Santa is dumb sjdfijblasbdjfihsjdhfbiksdhbfk",
+    "hriasehbfisbhdf9bsdifbsodbfib",
+    "idk",
+    "bruh"
+];
+
+let currentLine = 0;
+let currentChar = 0;
+
+function mouseClicked() {
+
+}
+
+function draw() {
+    background(255);
+
+    fill("#2a2a2aff");
+    rect(0, height - 80, width, 80);
+    fill("#fff");
+    textAlign(CENTER);
+    let modified = dialogue[currentLine].slice(0, currentChar);
+    text(modified, width / 2, height - 40);
+
+    if (frameCount % 3 == 0) {
+        currentChar++;
+    }
+
+    if (kb.pressed('Enter')) {
+        if (currentLine < dialogue.length - 1) {
+            currentLine++;
+            currentChar = 0;
+        } else {
+            window.location.href = "/adrianlvl.html";
+        }
+    }
+}
